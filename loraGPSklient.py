6eec895849f2500aa9699aaa7f2055123435839b
Haymount <mail@mathiashojbjerg.dk>
2022-02-16 12:51:57 +0100

    kode det printer gpgga commandoer

--- loraGPSklient.py
+++ loraGPSklient.py
@@ -1,11 +1,16 @@
 def main():
     while True:
         with open("/dev/ttyS0","r") as f:
-            data = f.read()
+            data = f.read(500)
             commands = data.split("\n")
             for command in commands:
                 args = command.split(",")
                 if args[0] == "$GPGGA":
                     print(args)
 
+def position():
+    
+
+
 main()
+
